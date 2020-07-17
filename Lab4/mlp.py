00592570c168a8c552e58bfeb1df558d48abe90a
Locietta <2324566152@qq.com>
2020-07-18 00:23:31 +0800

    Lab4 done

--- Lab4/mlp.py
+++ Lab4/mlp.py
@@ -1,29 +1,29 @@
 #%tensorflow_version 1.x
 import tensorflow as tf
-
 from tensorflow.examples.tutorials.mnist import input_data
+
+print(tf.__version__)
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 #通过tf.placeholder定义图片的输入和标签,
 # 图片为28x28的一维向量，标签为10分类的one hot
 #imgs shape: (n,784) labels shape: (n,10)
-x = 
-gt_y = 
-
+x = tf.placeholder(tf.float32, [None,784])
+gt_y = tf.placeholder(tf.float32, [None, 10])
 #通过tf.Variable定义mlp的wx+b中参数w和b
-W = 
-b = 
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
 
 #通过tf.matmul计算矩阵乘法
-pred_y =
+pred_y =tf.matmul(x, W)+b
 
 #通过tf.nn.softmax_cross_entropy_with_logits_v2定义交叉熵损失函数
-loss=
+loss= tf.nn.softmax_cross_entropy_with_logits_v2(labels=gt_y, logits = pred_y)
 #通过tf.reduce_mean将多个样本的损失求均值
-loss=
+loss= tf.reduce_mean(loss)
 #通过tf.train.GradientDescentOptimizer(lr).minimize(loss)定义学习率和梯度更新的方法
-optim =
+optim = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
 #初始化tensorflow
 with tf.Session() as sess:
@@ -38,8 +38,8 @@ with tf.Session() as sess:
         # batch_xs, batch_ys对应着两个占位符x和y_
         batch_xs, batch_ys = mnist.train.next_batch(100)
         # 在Session中运行train_step，运行时要传入占位符的值
-        #参考sess.run运行tensorflow，包括执行的占位符和输入数据，返回执行占位符的列表
-        =sess.run(, feed_dict={x: batch_xs, gt_y: batch_ys})
+        # 参考sess.run运行tensorflow，包括执行的占位符和输入数据，返回执行占位符的列表
+        _, step_loss = sess.run([optim,-tf.reduce_sum(gt_y * tf.log(pred_y+1e-10))], feed_dict={x: batch_xs, gt_y: batch_ys})
         if i%100==0:
           print('step {} loss: {:.2f}'.format(i,step_loss))
 
@@ -49,5 +49,5 @@ with tf.Session() as sess:
     #计算精度
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     #输入精度的占位符和喂给测试数据
-    acc_res=sess.run(accuracy, feed_dict={x: mnist.test.images, gt_y: mnist.test.labels}) # 0.8712
+    acc_res=sess.run(accuracy, feed_dict={x: mnist.test.images, gt_y: mnist.test.labels}) # 0.9194
     print(acc_res)
